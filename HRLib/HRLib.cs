016e6af10cf29d95e23d78d845396782073064ed
kand7dev <kand7dev@gmail.com>
2024-02-27 21:13:54 +0200

    Implement ValidName method

---
+++ HRLib/HRLib.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class HRLib
+{
+    public static void Echo()
+    {
+        Console.WriteLine("You're calling this class!");
+    }
+    public static bool ValidName(string Name)
+    {
+        string[] employeeNameAndSurname = Name.Split(' ');
+        foreach (string name in employeeNameAndSurname)
+        {
+            foreach (char character in name)
+            {
+                if (!char.IsLetter(character))
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
