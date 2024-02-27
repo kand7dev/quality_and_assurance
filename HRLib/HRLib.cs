c8dda1674a112ab8cc3c46d54b66b85848688ba1
kand7dev <kand7dev@gmail.com>
2024-02-27 21:27:24 +0200

    Implement CheckPhone method

--- HRLib/HRLib.cs
+++ HRLib/HRLib.cs
@@ -36,6 +36,53 @@ public class HRLib
         int key = 5;
         EncryptedPW = Encipher(Password, key);
     }
+    public static void CheckPhone(string Phone, ref int TypePhone, ref string? InfoPhone)
+    {
+        if (Phone.Length > 10)
+        {
+            TypePhone = -1;
+            InfoPhone = null;
+            return;
+        }
+        Dictionary<string, string> cellphoneDict = new Dictionary<string, string>
+        {
+            { "690", "NOVA" },
+            { "693", "NOVA" },
+            { "699", "NOVA" },
+            { "694", "VODAFONE" },
+            { "695", "VODAFONE" },
+            { "697", "COSMOTE" },
+            { "698", "COSMOTE" }
+        };
+        Dictionary<string, string> landlineDict = new Dictionary<string, string>
+        {
+            { "21", "Μητροπολιτική Περιοχή Αθήνας - Πειραιά" },
+            { "22", "Ανατολική Στερεά Ελλάδα, Αττική, Νησιά Αιγαίου" },
+            { "23", "Κεντική Μακεδονία" },
+            { "24", "Θεσσαλία, Δυτική Μακεδονία"},
+            { "25", "Θράκη, Ανατολική Μακεδονία" },
+            { "26", "Ήπειρος, Δυτική Στερεά Ελλάδα, Δυτική Πελοπόννησος, Ιόνια Νησιά" },
+            { "27", "Ανατολική Πελοπόννησος, Κύθηρα" },
+            { "28", "Κρήτη" }
+        };
+        string firstThreeDigits = Phone.Substring(0, 3); // or string firstThreeDigits = Phone[0..3]; which works only on C# 8.0
+        string firstTwoDigits = Phone.Substring(0, 2);
+        if (cellphoneDict.ContainsKey(firstThreeDigits))
+        {
+            TypePhone = 1;
+            InfoPhone = cellphoneDict[firstThreeDigits];
+        }
+        else if (landlineDict.ContainsKey(firstTwoDigits))
+        {
+            TypePhone = 0;
+            InfoPhone = landlineDict[firstTwoDigits];
+        }
+        else
+        {
+            TypePhone = -1;
+            InfoPhone = null;
+        }
+    }
 
     // Helper functions for ValidPassword.
     private static bool IsLetter(char character)
