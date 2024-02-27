2123e6e6fd9d4cb614ad019a556a3e5d514c3324
kand7dev <kand7dev@gmail.com>
2024-02-27 21:12:50 +0200

    Add project files.

---
+++ HRLib/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class HRLib
+{
+    public static void Echo()
+    {
+        Console.WriteLine("You're calling this class!");
+    }
+}
